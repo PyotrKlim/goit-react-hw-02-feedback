eee8795a9ee711e822fefa393b87e9c7b9c962f2
PyotrKlim <113846380+PyotrKlim@users.noreply.github.com>
2023-05-12 02:36:05 +0300

    styles added

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,12 @@
 import PropTypes from 'prop-types';
+import css from './Options.module.css';
 export const Options = ({ options, onLeaveFeedback }) => {
   return (
     <>
       {options.map(option => {
         return (
           <button
+            className={css.button}
             type="button"
             onClick={() => onLeaveFeedback(option)}
             key={option}
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,26 @@
 import PropTypes from 'prop-types';
+import css from './Statistics.module.css';
 export const Statistics = ({ good, neutral, bad, total, positive }) => {
   return (
-    <ul>
+    <ul className={css.statistics}>
       <li>
-        <span>good:</span>
+        <span>good: </span>
         <span>{good}</span>
       </li>
       <li>
-        <span>neutral:</span>
+        <span>neutral: </span>
         <span>{neutral}</span>
       </li>
       <li>
-        <span>bad:</span>
+        <span>bad: </span>
         <span>{bad}</span>
       </li>
       <li>
-        <span>total:</span>
+        <span>total: </span>
         <span>{total}</span>
       </li>
       <li>
-        <span>positive:</span>
+        <span>positive: </span>
         <span>{positive}%</span>
       </li>
     </ul>

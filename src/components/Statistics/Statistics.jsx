e1955d482d9a174e6369cb9ba832828b1c1237e3
PyotrKlim <113846380+PyotrKlim@users.noreply.github.com>
2023-05-18 16:05:25 +0300

    form and filter and contactList

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
+
 export const Statistics = ({ good, neutral, bad, total, positive }) => {
   return (
     <ul className={css.statistics}>
